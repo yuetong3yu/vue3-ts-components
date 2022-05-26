e1246bed91215851cf63b52624578b44efefef95
yuetong3yu <yuetong3yu@gmail.com>
2022-05-26 15:14:19 +0800

    tailwindcss ColumnList

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
   <fragment>
-    <column-list></column-list>
+    <column-list :list="columnList" />
   </fragment>
 </template>
 
-<script>
+<script lang="ts">
+import './App.css'
+
 import ColumnList from './components/ColumnList/index.vue'
+import type { ColumnProps } from './components/ColumnList/index.vue'
+
+const mockData: ColumnProps[] = [
+  {
+    id: String(Math.random()),
+    title: 'Column 1',
+    description: 'Description 1',
+    avatar: 'https://picsum.photos/200/200',
+  },
+  {
+    id: String(Math.random()),
+    title: 'Column 2',
+    description: 'Description 2',
+    avatar: 'https://picsum.photos/200/200',
+  },
+  {
+    id: String(Math.random()),
+    title: 'Column 3',
+    description: 'Description 3',
+    avatar: 'https://picsum.photos/200/200',
+  },
+  {
+    id: String(Math.random()),
+    title: 'Column 4',
+    description: 'Description 4',
+    avatar: 'https://picsum.photos/200/200',
+  },
+]
 
 export default {
   components: {
     ColumnList,
+  },
+  setup() {
+    return {
+      columnList: mockData,
+    }
   },
 }
 </script>
--- src/components/ColumnList/index.vue
+++ src/components/ColumnList/index.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>Column List</div>
+  <ul v-if="list && list.length" class="w-1/2 grid grid-cols-3">
+    <li
+      v-for="columnItem in list"
+      :key="columnItem.id"
+      class="w-48 p-3 m-3 bg-slate-100 shadow-lg flex flex-col items-center"
+    >
+      <img
+        :src="columnItem.avatar"
+        :alt="columnItem.title"
+        class="w-36 rounded-2xl"
+      />
+      <h4 class="text-blue-500 font-semibold">Title: {{ columnItem.title }}</h4>
+      <p class="text-gray-600">{{ columnItem.description }}</p>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+
+export interface ColumnProps {
+  id: string
+  title: string
+  description?: string
+  avatar: string
+}
 
 export default defineComponent({
-  setup() {
-    console.log('hello word1')
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      require: true,
+    },
+  },
+  setup(props) {
+    const { list } = props
+    return { list }
   },
 })
 </script>
